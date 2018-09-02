03bdd64f7a1254d5f3f0cefeae3bcee317ccdfdd
zut <ahilton@gmail.com>
2018-09-02 13:33:20 +1000

    basic thermometer styling

--- src/component/Thermometer.js
+++ src/component/Thermometer.js
@@ -1,14 +1,5 @@
-
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import {
-    Button,
-    Container, Grid, Image,
-    Menu,
-    Responsive,
-    Segment,
-    Visibility,
-} from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Grid, Label, Segment,} from 'semantic-ui-react'
 
 
 class Thermometer extends Component {
@@ -20,29 +11,71 @@ class Thermometer extends Component {
         return (
 
             <Segment basic className='thermometerSegment'>
+                <Grid centered container >
+                    <Grid.Row>
+                        <Grid.Column textAlign='center' width={3}>
+                            <h1>Total Raised $1000</h1>
+                        </Grid.Column>
+                        <Grid.Column textAlign='center' stretched width={10}>
+                            <h1>Target $350,000</h1>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
                 <Grid stretched centered container className='thermometerGrid'>
-                    <Grid.Column className='thermometerThinColumn' stretched width={3}>
-                        <div style={{height:'10%', backgroundColor:'yellow'}}>
-                            <div style={{
-                                width:'300px',
-                                border:'1px solid blue',
-                                position:'absolute'
+
+                    <Grid.Row>
+                    <Grid.Column textAlign='center' className='thermometerColumn' stretched width={3}>
+
+
+
+                        <div className='thermometerContainer' style={{
+
+
+                        }}>
+
+                            <Segment basic className='thermometerBlock' style={{
+                                height: '20%',
+                                //padding:0
+                            }}>
+                                <div style={{
+                                    width:'300px',
+                                    height: 5,
+                                    minHeight: 5,
+                                    backgroundColor:'blue',
+                                    position:'absolute',
+                                    left:50
+                                }}/>
+                                <Label as='a'  size='large' color='black' ribbon>
+                                    Donations
+                                </Label>
+
+                            </Segment>
+
+                            <Segment basic  className='thermometerBlock' style={{
+                                height: '30%'
                             }}>
 
-                            </div>
+                            </Segment>
 
-                        </div>
+                            <Segment basic className='thermometerBlock' style={{
+                                height: '50%'
+                            }}>
 
-                        <div style={{height:'90%', backgroundColor:'green'}}>
+                            </Segment>
 
-                        </div>
-
-                    </Grid.Column>
-                    <Grid.Column className='thermometerFatColumn' stretched width={10}>
-                        <div style={{height:'100%', backgroundColor:'green'}}>
-
+                            <Segment circular className='thermometerBlockBase'>
+                            </Segment>
                         </div>
                     </Grid.Column>
+                    <Grid.Column className='thermometerDetailColumn' stretched width={10}>
+                        <div style={{
+                            height: '100%',
+                            backgroundColor: '#bbb'
+                        }}>
+
+                        </div>
+                    </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Segment>
         )
